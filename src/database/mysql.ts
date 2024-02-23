52c063f715e5b293e4f8a2da2e0ecec36d876243
PJ Lenhart <lenhartpj@gmail.com>
2024-02-23 08:29:12 -0500

    playing around

--- src/database/mysql.ts
+++ src/database/mysql.ts
@@ -1,12 +1,26 @@
 import config from './config';
+import mysql from 'mysql';
 import { createPool } from 'mysql2';
 
-// const params = {
-//   user: config.mysql.user,
-//   password: config.mysql.pass,
-//   host: config.mysql.host,
-//   database: config.mysql.database,
-// };
+export const connection1 = mysql.createConnection({
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.pass,
+    database: config.mysql.database,
+    port: 3306,
+});
+
+connection1.connect((err) => {
+    if (err) throw err;
+    console.log('You are now connected to the DB');
+});
+
+const params = {
+    user: config.mysql.user,
+    password: config.mysql.pass,
+    host: config.mysql.host,
+    database: config.mysql.database,
+};
 
 export const connection = createPool({
     user: config.mysql.user,
--- src/routes/wrestlerRouter.ts
+++ src/routes/wrestlerRouter.ts
@@ -1,63 +1,69 @@
 import { Request, Response } from 'express';
-import { connection } from '../database/mysql';
-import { QueryError, PoolConnection } from 'mysql2';
+//import { connection } from '../database/mysql';
+//import { QueryError, PoolConnection } from 'mysql2';
+import { connection1 } from '../database/mysql';
 
 const express = require('express');
 
 const wrestlerRouter = express.Router();
 
-wrestlerRouter.get('/', async (req: Request, res: Response) => {
-    let query = 'SELECT * FROM wrestlingdb.wrestling_wrestler';
-    connection.getConnection(
-        (err: NodeJS.ErrnoException, conn: PoolConnection) => {
-            if (err) {
-                res.status(500).send({
-                    message: err.message,
-                    data: null,
-                });
-            }
-            conn.query(query, (err: QueryError, resultSet: any) => {
-                conn.release();
-                if (err) {
-                    res.status(500).send({
-                        message: err.message,
-                        data: null,
-                    });
-                } else {
-                    res.status(200).send({
-                        message: 'Successfully retrieved all wrestlers',
-                        data: resultSet,
-                    });
-                }
-            });
-        }
-    );
+wrestlerRouter.get('/', (req: Request, res: Response) => {
+    let getQuery = 'SELECT * FROM wrestlingdb.wrestling_wrestler';
+    connection1.query(getQuery, (error, results, fields) => {
+        if (error) throw error;
+        res.end(JSON.stringify(results));
+    });
+    // connection.getConnection(
+    //     (err: NodeJS.ErrnoException, conn: PoolConnection) => {
+    //         conn.release();
+    //         if (err) {
+    //             res.status(500).send({
+    //                 message: err.message,
+    //                 data: null,
+    //             });
+    //         }
+    //         conn.query(query, (err: QueryError, resultSet: any) => {
+    //             conn.release();
+    //             if (err) {
+    //                 res.status(500).send({
+    //                     message: err.message,
+    //                     data: null,
+    //                 });
+    //             } else {
+    //                 res.status(200).send({
+    //                     message: 'Successfully retrieved all wrestlers',
+    //                     data: resultSet,
+    //                 });
+    //             }
+    //         });
+    //     }
+    // );
 });
 
-wrestlerRouter.get('/:id', async (req: Request, res: Response) => {
-    let query = `SELECT * FROM wrestlingdb.wrestling_wrestler w WHERE w.wrestler_id=${req.params.id}`;
-    connection.getConnection((err: QueryError, conn: PoolConnection) => {
-        if (err) {
-            res.status(500).send({
-                message: err.message,
-                data: null,
-            });
-        }
-        conn.query(query, (err, resultSet: any) => {
-            conn.release();
-            if (err) {
-                res.status(500).send({
-                    message: err.message,
-                    data: null,
-                });
-            } else {
-                res.status(200).send({
-                    message: `Successfully retrieved wrestler with id: ${req.params.id}`,
-                    data: resultSet,
-                });
-            }
-        });
-    });
-});
+// wrestlerRouter.get('/:id', async (req: Request, res: Response) => {
+//     let query = `SELECT * FROM wrestlingdb.wrestling_wrestler w WHERE w.wrestler_id=${req.params.id}`;
+//     connection.getConnection((err: QueryError, conn: PoolConnection) => {
+//         if (err) {
+//             res.status(500).send({
+//                 message: err.message,
+//                 data: null,
+//             });
+//         }
+//         conn.query(query, (err, resultSet: any) => {
+//             conn.release();
+//             if (err) {
+//                 res.status(500).send({
+//                     message: err.message,
+//                     data: null,
+//                 });
+//             } else {
+//                 res.status(200).send({
+//                     message: `Successfully retrieved wrestler with id: ${req.params.id}`,
+//                     data: resultSet,
+//                 });
+//             }
+//         });
+//     });
+// });
 
 export default wrestlerRouter;
