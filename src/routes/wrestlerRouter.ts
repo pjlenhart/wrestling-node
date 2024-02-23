9c808751d940e1f67e7671a76c1b2f5e023d0500
PJ Lenhart <lenhartpj@gmail.com>
2024-02-23 10:28:32 -0500

    fixing something maybe

--- src/routes/wrestlerRouter.ts
+++ src/routes/wrestlerRouter.ts
@@ -8,17 +8,13 @@ const express = require('express');
 const wrestlerRouter = express.Router();
 
 wrestlerRouter.get('/', async (req: Request, res: Response) => {
-    let query = 'SELECT * FROM wrestlingdb.wrestling_wrestler';
     connection.getConnection(
         (err: NodeJS.ErrnoException, conn: PoolConnection) => {
             if (err) throw err;
+            let query: string = 'SELECT * FROM wrestlingdb.wrestling_wrestler';
             conn.query(query, (err: QueryError, resultSet: any) => {
-                if (err) {
-                    res.status(500).send({
-                        message: err.message,
-                        data: null,
-                    });
-                } else {
+                if (err) throw err;
+                else {
                     res.status(200).send({
                         message: 'Successfully retrieved all wrestlers',
                         data: resultSet,
@@ -35,12 +31,8 @@ wrestlerRouter.get('/:id', async (req: Request, res: Response) => {
     connection.getConnection((err: QueryError, conn: PoolConnection) => {
         if (err) throw err;
         conn.query(query, (err, resultSet: any) => {
-            if (err) {
-                res.status(500).send({
-                    message: err.message,
-                    data: null,
-                });
-            } else {
+            if (err) throw err;
+            else {
                 res.status(200).send({
                     message: `Successfully retrieved wrestler with id: ${req.params.id}`,
                     data: resultSet,
